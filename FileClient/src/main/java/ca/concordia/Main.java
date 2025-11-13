f85e590e370f7905e184b44cf7befbad5436cc33
Rohan-T27 <rohan.thangarajah@outlook.com>
2025-11-13 16:36:55 -0500

    Update Main.java

--- FileClient/src/main/java/ca/concordia/Main.java
+++ FileClient/src/main/java/ca/concordia/Main.java
@@ -1,5 +1,5 @@
 package ca.concordia;
-//test
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -7,8 +7,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         //Socket CLient
